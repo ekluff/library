0675d90297d66f6b8d8bd3f5cc627c4d457cae5a
Michael Bunker & Evan Clough <student@epicodus.com>
2015-08-19 11:15:16 -0700

    refactor spec_helper to connect to new library database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,12 @@ require('authors')
 require('checkout')
 require('pg')
 
-DB = PG.connect({:dbname => 'to_do_test'})
+DB = PG.connect({:dbname => 'library_test'})
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec("DELETE FROM tasks *;")
-    DB.exec("DELETE FROM lists *;")
+    DB.exec("DELETE FROM books *;")
+    DB.exec("DELETE FROM patrons *;")
+    DB.exec("DELETE FROM authors *;")
   end
 end
