afb98bd9a4c1099c8a9855c4bfb3f8fdf312c430
Michael Bunker & Evan Clough <student@epicodus.com>
2015-08-19 11:39:17 -0700

    Create functionality to save book to database.

--- lib/books.rb
+++ lib/books.rb
@@ -4,7 +4,7 @@ class Books
 
   define_method(:initialize) do |attributes|
     @title = attributes.fetch(:title)
-    @id = attributes.fetch(:id)
+    @id = attributes.fetch(:id, nil)
   end
 
   define_singleton_method(:all) do
@@ -12,13 +12,20 @@ class Books
     books = []
     returned_books.each do |book|
       title = book.fetch('title')
-      id = book.fetch('id')
+      id = book.fetch('id').to_i
       books.push(Books.new({:title => title, :id => id}))
     end
     books
   end
 
+  def save
+    result = DB.exec("INSERT INTO books (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
 
+  def ==(another_book)
+    self.id == another_book.id
+  end
 
 
 
--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -1,10 +1,19 @@
 require('spec_helper')
 
 describe(Books) do
+
   describe('.all') do
     it('returns empty book array') do
       expect(Books.all()).to(eq([]))
     end
   end
-  
+
+  describe('#save') do
+    it('saves a new book into the database') do
+      book = Books.new({title: 'To Kill a Mockingbird'})
+      book.save
+      expect(Books.all).to(eq([book]))
+    end
+  end
+
 end
